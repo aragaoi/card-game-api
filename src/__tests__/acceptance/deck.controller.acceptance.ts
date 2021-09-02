cffe3c400824c9a6f5d9db03231f220b9814e07e
iaragao <irenio.limaj@hotmail.com>
2021-09-02 09:00:21 -0300

    Create acceptance tests for deck controller

---
+++ src/__tests__/acceptance/deck.controller.acceptance.ts
@@ -0,0 +1,99 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import {Client, expect} from '@loopback/testlab';
+import {CardGameApiApplication} from '../..';
+import {setupApplication} from './test-helper';
+import {DeckRepository} from "../../repositories";
+
+const uuidPattern = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
+
+describe('DeckController', () => {
+  let app: CardGameApiApplication;
+  let client: Client;
+  let deckRepository: DeckRepository;
+
+  before('setupApplication', async () => {
+    ({app, client} = await setupApplication());
+    deckRepository = await app.getRepository(DeckRepository);
+  });
+
+  after(async () => {
+    await app.stop();
+  });
+
+  describe('POST /decks', () => {
+    it('should create a new deck with the specified properties', async () => {
+      const res = await client.post('/decks')
+        .send({
+          deck_id: "521b0293-01f7-44c2-9990-27079eb2352d",
+          shuffled: false,
+          remaining: 3
+        })
+        .expect('Content-Type', /json/)
+        .expect(201);
+
+      expect(res.body).to.be.eql({
+        deck_id: "521b0293-01f7-44c2-9990-27079eb2352d",
+        shuffled: false,
+        remaining: 3
+      });
+    });
+
+    it('should create a new deck with default values', async () => {
+      const res = await client.post('/decks')
+        .send({})
+        .expect('Content-Type', /json/)
+        .expect(201);
+
+      expect(res.body?.deck_id).to.match(uuidPattern);
+      expect(res.body).to.containEql({
+        shuffled: false,
+        remaining: 52
+      });
+    });
+  });
+
+  describe('GET /decks', () => {
+
+    before('populate decks', async () => {
+      await deckRepository.create({
+        deck_id: 'c64fb35c-f117-4219-a921-131c3bbd5857',
+        shuffled: false,
+        remaining: 3
+      });
+    });
+
+    it('should return the specified deck', async () => {
+      const deckUUID = 'c64fb35c-f117-4219-a921-131c3bbd5857';
+      const res = await client.get(`/decks/${deckUUID}`)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(res.body).to.containEql({
+        deck_id: 'c64fb35c-f117-4219-a921-131c3bbd5857',
+        shuffled: false,
+        remaining: 3,
+      });
+      expect(res.body?.cards).to.be.length(3);
+    });
+
+    it('should return error for missing deck_id', async () => {
+      const deckUUID = '';
+      const res = await client.get(`/decks/${deckUUID}`)
+        .expect('Content-Type', /json/)
+        .expect(404);
+
+      expect(res.body).to.be.empty();
+    });
+
+    it('should return error for invalid deck_id', async () => {
+      const deckUUID = 'abcd1234-qwert9876';
+      const res = await client.get(`/decks/${deckUUID}`)
+        .expect('Content-Type', /json/)
+        .expect(400);
+
+      expect(res.body).to.be.equal({
+        error: 'The deck id should be a valid UUID.'
+      });
+    });
+  });
+});
