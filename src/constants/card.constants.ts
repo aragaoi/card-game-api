887caf36439ef52b1bcfbeb1da4bf2cc4a19e5c9
iaragao <irenio.limaj@hotmail.com>
2021-09-05 00:01:00 -0300

    Implement card helper to build the cards

---
+++ src/constants/card.constants.ts
@@ -0,0 +1,2 @@
+export const FRENCH_CARDS_SUITS = ['SPADES', 'CLUBS', 'DIAMONDS', 'HEARTS'];
+export const FRENCH_CARDS_VALUES = ['ACE', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'VALET', 'DAME', 'KING'];
--- src/helpers/card-type.helper.ts
+++ src/helpers/card-type.helper.ts
@@ -1,7 +1,19 @@
 import {Card} from "../models";
+import {FRENCH_CARDS_SUITS, FRENCH_CARDS_VALUES} from "../constants/card.constants";
 
 const buildCardDeck = (): Card[] => {
-  return [];
+  const cards: Card[] = [];
+
+  FRENCH_CARDS_SUITS.forEach(suit =>
+    FRENCH_CARDS_VALUES.forEach(value => {
+      cards.push(new Card({
+        value,
+        suit,
+        code: `${value[0]}${suit[0]}`
+      }))
+    })
+  );
+  return cards;
 };
 
 export {buildCardDeck};
